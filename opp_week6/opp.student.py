ecbbea2a41b2df9234e54f9cf62d6472ce11c162
chutima-tnk <67319010026@tnk.ac.th>
2024-11-22 16:17:57 +0700

    Update opp.student.py

--- opp_week6/opp.student.py
+++ opp_week6/opp.student.py
@@ -4,11 +4,17 @@ class student:
         self.name = ชื่อนามสกุล
         self.nickname = ชื่อเล่น
         self.score = random.randint(1,10)
+        self.editscore = random.randint(1,10)
     def Scores(self):
         if self.score >=5:
             print(f"{self.name} {self.nickname} {self.score} : คุณสอบผ่าน")
         else :
             print(f"{self.name} {self.nickname} {self.score} : คุณสอบตก")
+            print("--------------สอบตก-----------")
+            if self.editscore >=5:
+                print(f"{self.name} {self.nickname} {self.editscore} :ผ่าน")
+            else:
+                 print(f"{self.name} {self.nickname} {self.score} : ตก")
 student1 =student("Pinthip Rungsri","pin",)
 student2 =student("nanticha wongyong","ice",)
 student3 =student("Saowalak Pankaew","Taengkwa",) 
